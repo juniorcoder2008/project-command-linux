ebaffe556a7213441ff39ad5753e47dc40da14eb
Junior Coder <57005033+juniorcoder2008@users.noreply.github.com>
2021-02-20 14:06:14 +0100

    Initial Commit

--- create-project.cmd
+++ create-project.cmd
@@ -1,1 +1,14 @@
-git clone https://github.com/juniorcoder2008/dev-env.git && mv ./dev-env/ ./%1/ && cd %1 && npm install && code . && gulp && npm start 
+#!/bin/bash
+
+echo Give your project a name:
+read name
+
+git clone https://github.com/juniorcoder2008/dev-env.git 
+mv ./dev-env/ ./$name/ 
+cd $name 
+
+npm install 
+
+code . 
+gulp
+npm start 
